591d2cf729003d82a7e50c4634ef0dadfa8e6b0b
Kassymzhan Zhandos <zandoskasenov@gmail.com>
2023-05-20 02:27:12 +0600

    добавил счетчик с помощю хука useStat

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ import PizzaBlock from './components/PizzaBlock';
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
     <Header/>
-    <div class="content">
-      <div class="container">
-        <div class="content__top">
+    <div className="content">
+      <div className="container">
+        <div className="content__top">
         <Categories/>
           <Sort/>
         </div>
-        <h2 class="content__title">Все пиццы</h2>
-        <div class="content__items">
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">
      <PizzaBlock title="Мексиканская" price={500} image={"https://cdn.carte.by/assets/2020/11/06/0ce50166fb8d20c0148322270f36182c-6b93b_src---jpg_710x_83b40_convert.jpg"} /> 
      <PizzaBlock title="Пепирони" price={655} image={"https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"}  />
      <PizzaBlock title="Маргарита" price={455} image={"https://media-cdn.tripadvisor.com/media/photo-s/1a/e9/94/10/caption.jpg"}/> 
--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,28 +1,36 @@
-import React from "react"
+import React from "react";
 
-function PizzaBlock(Props) {
+
+function PizzaBlock({image, title, price}) {
+
+  const [pizzaCount, setPizzaCount] =React.useState(0);
+  const onClickAdd=()=>{
+    setPizzaCount(pizzaCount+1);
+  };
+  
+
   return(
-    <div class="pizza-block">
+    <div className="pizza-block">
     <img
-      class="pizza-block__image"
-      src={Props.image}
+      className="pizza-block__image"
+      src={image}
       alt="Pizza"
     />
-    <h4 class="pizza-block__title">{Props.title}</h4>
-    <div class="pizza-block__selector">
+    <h4 className="pizza-block__title">{title}</h4>
+    <div className="pizza-block__selector">
       <ul>
-        <li class="active">тонкое</li>
+        <li className="active">тонкое</li>
         <li>традиционное</li>
       </ul>
       <ul>
-        <li class="active">26 см.</li>
+        <li className="active">26 см.</li>
         <li>30 см.</li>
         <li>40 см.</li>
       </ul>
     </div>
-    <div class="pizza-block__bottom">
-      <div class="pizza-block__price">от {Props.price} ₽</div>
-      <div class="button button--outline button--add">
+    <div className="pizza-block__bottom">
+      <div className="pizza-block__price">от {price} ₽</div>
+      <div onClick={onClickAdd} className="button button--outline button--add">
         <svg
           width="12"
           height="12"
@@ -36,7 +44,7 @@ function PizzaBlock(Props) {
           />
         </svg>
         <span>Добавить</span>
-        <i>2</i>
+        <i>{pizzaCount}</i>
       </div>
     </div>
     </div> 
